6443a7991ba76d0cd2aba582f95be3c41ba7e24d
arestus <arestus@gmail.com>
2021-04-23 15:09:33 +0300

    add detailPage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,27 +1,39 @@
 import React,  { useState, useEffect } from 'react'
-import { fetchGenre, fetchMovies } from '../../services'
-import ReactDOM from 'react-dom'
+import { fetchGenre, fetchMovies, fetchMovieByGenre,fetchPersons, fetchTopratedMovie } from '../../services'
+// import ReactDOM from 'react-dom'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel';
 import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css'
+import { Link } from 'react-router-dom'
+import ReactStars from 'react-rating-stars-component'
 // import RBCarousel from "react-bootstrap-carousel";
 
 export function Home() {
     const [nowPlaying, setNowPlaying] = useState([]);
     const [genres, setGenres] = useState([]);
-
+    const [movieByGenre, setMovieByGenre] = useState([]);
+    const [persons, setPersons] = useState([]);
+    const [topRated, setTopRated] = useState([]);
+    
     useEffect(() => {
         const fetchAPI = async () => {
-            setNowPlaying(await fetchMovies())
-            setGenres(await fetchGenre())
+            setNowPlaying(await fetchMovies());
+            setGenres(await fetchGenre());
+            setMovieByGenre(await fetchMovieByGenre(28));
+            setPersons(await fetchPersons())
+            setTopRated(await fetchTopratedMovie())
         };
         fetchAPI();
     }, []);
     
+    const handleGenreClick = async (genre_id) => {
+        setMovieByGenre(await fetchMovieByGenre(genre_id));
+}
+    
     const movies = nowPlaying.slice(0, 5).map((item, index) => {
         return (
-            <div style={{height: 500, width: '100%'}} key={index}>
-                <div className="carousel-center">
+            <div style={{height: 500, width: "100%"}} key={index}>
+                <div className="carousel">
                     <img style={{ height: 600}} src={item.backPoster} alt={item.title} />
                 </div>
                 <div className="carousel-center">
@@ -37,10 +49,60 @@ export function Home() {
     const genreList = genres.map((item, index) => {
         return (
             <li className="list-inline-item" key={index}>
-                <button type="button" className="btn btn-outline-info">
+                <button type="button" className="btn btn-outline-info" onClick={() => {
+                    handleGenreClick(item.id)
+                }}>
                     {item.name}
                 </button>
         </li>
+    )
+    })
+    
+    const movieList = movieByGenre.slice(0, 4).map((item, index) => {
+        return (
+            <div className='col-md-3 col-sm-6' key={index}>
+                <div className='card'>
+                    <Link to={`/movie/${item.id}`}>
+                        <img className="img-fluid" src={item.poster} alt={item.title}></img>
+                    </Link>
+                </div>
+                <div className='mt-3'>
+                    <p style={{ fontWeight: 'bolder' }}>Rated: {item.title}</p>
+                    <ReactStars count={item.rating} size={20} color1={'#f4c10f'}
+                        ></ReactStars>
+                </div>
+            </div>
+        )
+    })
+
+    const trendingPersons = persons.slice(0, 4).map((p, i) => {
+        return (
+            <div className="col-md-3 text-center" key={i}>
+                <img className="img-fluid rounded-circle mx-auto d-block" src={p.profileImg} alt={p.name}/>
+                    <p className='font-weight-bold text-center'>
+                        {p.name}
+                </p>
+                <p className="font-weight-light text-center" style={{ color: '#5a606b' }}>
+                    Trending for {p.known}
+                </p>
+            </div>
+        )
+    });
+
+    const topRatedList = topRated.slice(0, 4).map((item, index) => {
+        return (
+            <div className="col-md-3" key={index}>
+                <div className='card'>
+                    <Link to={`/movies/${item.id}`}>
+                        <img className="img-fluid" src={item.poster} alt={item.title}/>
+                    </Link>
+                </div>
+                <div className='mt-3'>
+                    <p style={{ fontWeight: 'bolder' }}>Rated: {item.title}</p>
+                    <ReactStars count={item.rating} size={20} color1={'#f4c10f'}
+                        ></ReactStars>
+                </div>
+        </div>
     )
 })
 
@@ -54,6 +116,7 @@ export function Home() {
                         slidesshowSpeed={5000}
                         version={4}
                         indicators={false}
+                        showThumbs={false} 
                     >
                         {movies}
                     </Carousel>
@@ -67,6 +130,101 @@ export function Home() {
                     </ul>
                 </div>
             </div>
-       </div>
-   )
+            <div className="row mt-3">
+                <div className="col">
+                    <div className="float-right">
+                        <i className="far fa-arrow-alt-circle-right"></i>
+                    </div>
+                </div>
+</div>
+            <div className="row mt-3">
+                {movieList}
+            </div>
+
+            <div className='row mt-3'>
+                <div className='col'>
+                    <p className='font-weight-bold' style={{ color: '#5a606b' }}>
+                        TRENDING PERSONS THIS WEEK
+                    </p>
+                </div>
+            </div>
+            <div className="row mt-3">
+                <div className="col">
+                    <div className="float-right">
+                        <i className="far fa-arrow-alt-circle-right"></i>
+                    </div>
+                </div>
+</div>
+            <div className="row mt-3">{trendingPersons}</div>
+            <div className="row mt-3">
+                <div className="col">
+                    <p className="font-weight-bold" style={{ color: '#5a606b' }}>
+                        TOP RATED MOVIES
+                    </p>
+                </div>
+            </div>
+            <div className="row mt-3">
+                <div className="col">
+                    <div className="float-right">
+                        <i className="far fa-arrow-alt-circle-right"></i>
+                    </div>
+                </div>
+</div>
+            <div className="row mt-3">{topRatedList}</div>
+            <hr className="mt-5" style={{ borderTop: '1px solid #5a606b' }}></hr>
+            
+            <div className='row mt-3 mb-5'>
+                <div className="col-md-8 col-sm-6" style={{ color: '#5a606b' }}>
+                    <h3>ABOUT ME</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, libero quos, aperiam sapiente exercitationem consequuntur fugiat, veniam tempore corrupti perspiciatis a accusantium deleniti! Dignissimos quis, cumque eos omnis ducimus voluptas?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quisquam, nisi voluptas nam, sit a, placeat debitis asperiores corrupti aut quos inventore dolorem excepturi explicabo quaerat quibusdam harum. Sapiente, iste!</p>
+                    <ul className="list-inline">
+                        <li className="list-inline-item">
+                            <a href="/" style={{ color: '#f4c10f' }}>
+                                <i className='fab fa-facebook'></i>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="/" style={{ color: '#f4c10f' }}>
+                                <i className='fab fa-youtube'></i>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="/" style={{ color: '#f4c10f' }}>
+                                <i className='fab fa-twitter'></i>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="/" style={{ color: '#f4c10f' }}>
+                                <i className='fab fa-instagram'></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-md-4 col-sm-6" style={{ color: '#5a606b' }}>
+                    <h3>KEEP IN TOUCH</h3>
+                    <ul className="list-unstyled">
+                        <li>
+                            <p>
+                                <strong>
+                                    <i className="fas fa-map-marker-alt"></i> Address:</strong> city, state, country
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <strong>
+                                    <i className="fas fa-phone"></i> Phone:</strong> +3 093 333-333-333
+                            </p>
+                        </li>
+                        <li>
+                            <p>
+                                <strong>
+                                    <i className="fas fa-envelope"></i> Email:</strong> info@infomail.com
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/services/index.js
+++ src/services/index.js
@@ -49,16 +49,129 @@ export const fetchGenre = async () => {
   } catch (error) {}
 };
 
-export const fetchMovieByGenre = () => {};
+export const fetchMovieByGenre = async (genre_id) => {
+  try {
+    const { data } = await axios.get(moviesUrl, {
+      params: {
+        api_key: apiKey,
+        language: "en_US",
+        page: 1,
+        with_genres: genre_id,
+      },
+    });
+    const posterUrl = "https://image.tmdb.org/t/p/original/";
+    const modifiedData = data["results"].map((m) => ({
+      id: m["id"],
+      backPoster: posterUrl + m["backdrop_path"],
+      popularity: m["popularith"],
+      title: m["title"],
+      poster: posterUrl + m["poster_path"],
+      overview: m["overview"],
+      rating: m["vote_average"],
+    }));
+    return modifiedData;
+  } catch (error) {}
+};
 
-export const fetchPersons = () => {};
+export const fetchPersons = async () => {
+  try {
+    const { data } = await axios.get(personUrl, {
+      params: {
+        api_key: apiKey,
+      },
+    });
+    const modifiedData = data["results"].map((p) => ({
+      id: p["id"],
+      popularity: p["popularity"],
+      name: p["name"],
+      profileImg: "https://image.tmdb.org/t/p/w200" + p["profile_path"],
+      known: p["known_for_department"],
+    }));
+    return modifiedData;
+  } catch (error) {}
+};
 
-export const fetchTopratedMovie = () => {};
+export const fetchTopratedMovie = async () => {
+  try {
+    const { data } = await axios.get(topratedUrl, {
+      params: {
+        api_key: apiKey,
+        language: "en_US",
+        page: 1,
+      },
+    });
+    const posterUrl = "https://image.tmdb.org/t/p/original/";
+    const modifiedData = data["results"].map((m) => ({
+      id: m["id"],
+      backPoster: posterUrl + m["backdrop_path"],
+      popularity: m["popularith"],
+      title: m["title"],
+      poster: posterUrl + m["poster_path"],
+      overview: m["overview"],
+      rating: m["vote_average"],
+    }));
+    return modifiedData;
+  } catch (error) {}
+};
 
-export const fetchMovieDetail = () => {};
+export const fetchMovieDetail = async (id) => {
+  try {
+    const { data } = await axios.get(`${movieUrl}/${id}`, {
+      params: {
+        api_key: apiKey,
+        language: "en_US",
+      },
+    });
+    return data;
+  } catch (error) {}
+};
 
-export const fetchMovieVideos = () => {};
+export const fetchMovieVideos = async (id) => {
+  try {
+    const { data } = await axios.get(`${movieUrl}/${id}/videos`, {
+      params: {
+        api_key: apiKey,
+      },
+    });
+    return data["results"][0];
+  } catch (error) {}
+};
 
-export const fetchCasts = () => {};
+export const fetchCasts = async (id) => {
+  try {
+    const { data } = await axios.get(`${movieUrl}/${id}/credits`, {
+      params: {
+        api_key: apiKey,
+      },
+    });
+    const modifiedData = data["cast"].map((c) => ({
+      id: c["cast"],
+      character: c["character"],
+      name: c["name"],
+      img: "https://image.tmdb.org/t/p/w200" + c["profile_path"],
+    }));
+    return modifiedData;
+  } catch (error) {}
+};
 
-export const fetchSimilarMovie = () => {};
+export const fetchSimilarMovie = async (id) => {
+  try {
+    const { data } = await axios.get(`${movieUrl}/${id}/similar`, {
+      params: {
+        api_key: apiKey,
+        language: "en_US",
+      },
+    });
+    const posterUrl = "https://image.tmdb.org/t/p/original/";
+    const modifiedData = data["results"].map((m) => ({
+      id: m["id"],
+      backPoster: posterUrl + m["backdrop_path"],
+      popularity: m["popularith"],
+      title: m["title"],
+      poster: posterUrl + m["poster_path"],
+      overview: m["overview"],
+      rating: m["vote_average"],
+    }));
+    return modifiedData;
+  } catch (error) {}
+};
